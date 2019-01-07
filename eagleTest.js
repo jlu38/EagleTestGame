3fc7d3a35984cc4ea850b3effa70df646f87a7af
jlu38 <joeylu38@gmail.com>
2019-01-06 22:43:02 -0500

    Added visible squares and functionality when squares are cilcked

--- eagleTest.js
+++ eagleTest.js
@@ -1,1 +1,23 @@
-console.log("Connected");
+var colors=[
+  "rgb(255, 0, 0)",
+  "rgb(255, 0, 0)",
+  "rgb(255, 0, 0)",
+  "rgb(0, 255, 0)"
+]
+
+var squares = document.querySelectorAll(".square");
+var pickedColor = colors[3];
+
+for(var i = 0; i < squares.length; i++){
+  squares[i].style.backgroundColor = colors[i];
+
+  squares[i].addEventListener("click", function(){
+    var clickedColor = this.style.backgroundColor;
+    if(pickedColor === clickedColor){
+      alert("Correct");
+    }
+    else{
+      alert("Wrong");
+    }
+  });
+}
