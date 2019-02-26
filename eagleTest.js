cbe73432261319259f73c1b756e76a103fdcba4c
Az Vasquez <azvasquez99@gmail.com>
2019-02-25 20:54:33 -0500

    finished product

--- eagleTest.js
+++ eagleTest.js
@@ -1,15 +1,15 @@
 var colors=[
-  "rgb(255, 0, 0)",
-  "rgb(255, 0, 0)",
-  "rgb(255, 0, 0)",
-  "rgb(0, 255, 0)"
+  "https://i.makeagif.com/media/9-27-2015/NnMrSO.gif",
+  "https://thumbs.gfycat.com/IcyJubilantCuscus-max-1mb.gif",
+  "https://thumbs.gfycat.com/IcyJubilantCuscus-max-1mb.gif",
+  "https://thumbs.gfycat.com/InnocentCheeryKitfox-small.gif"
 ]
 
 var squares = document.querySelectorAll(".square");
 var pickedColor = colors[3];
 
 for(var i = 0; i < squares.length; i++){
-  squares[i].style.backgroundColor = colors[i];
+  squares[i].style.backgroundImage = `url('${colors[i]}')`;
 
   squares[i].addEventListener("click", function(){
     var clickedColor = this.style.backgroundColor;
@@ -17,7 +17,7 @@ for(var i = 0; i < squares.length; i++){
       alert("Correct");
     }
     else{
-      alert("Wrong");
+      alert("Smoke weed everyday");
     }
   });
 }
